d7b06f1f628bfc6c46d03dc22affc8db0d8c4779
houdunren <2300071698@qq.com>
2023-04-24 23:01:15 +0800

    hdcms网站项目开始了

---
+++ hdcms/prisma/migrations/20230424141604_init/migration.sql
@@ -0,0 +1,28 @@
+-- CreateTable
+CREATE TABLE `User` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `name` VARCHAR(191) NOT NULL,
+    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(191) NULL,
+    `mobile` VARCHAR(191) NULL,
+    `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- CreateTable
+CREATE TABLE `App` (
+    `id` INTEGER NOT NULL AUTO_INCREMENT,
+    `title` VARCHAR(191) NOT NULL,
+    `content` TEXT NOT NULL,
+    `preview` VARCHAR(191) NOT NULL,
+    `createAt` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `updateAt` DATETIME(3) NOT NULL,
+    `userId` INTEGER NOT NULL,
+
+    PRIMARY KEY (`id`)
+) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+
+-- AddForeignKey
+ALTER TABLE `App` ADD CONSTRAINT `App_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `User`(`id`) ON DELETE CASCADE ON UPDATE CASCADE;
---
+++ hdcms/prisma/seed.ts
@@ -0,0 +1,23 @@
+import { PrismaClient } from '@prisma/client'
+import { Random } from 'mockjs'
+const prisma = new PrismaClient()
+
+async function run() {
+  for (let i = 0; i < 30; i++) {
+    await prisma.user.create({
+      data: {
+        name: Random.cname(),
+        password: Random.string(),
+        App: {
+          create: {
+            title: Random.csentence(),
+            content: Random.cparagraph(),
+            preview: Random.image('300x300'),
+          },
+        },
+      },
+    })
+  }
+}
+
+run()
